196c7c72cd106d67aa1a20f870b09d1f7004c43e
lazaronixon <lazaronixon@hotmail.com>
2015-02-20 18:59:45 -0200

    Initial commit

---
+++ test/test_helper.rb
@@ -0,0 +1,18 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
+
+class ActiveSupport::TestCase
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
+
+  # Add more helper methods to be used by all tests here...
+
+  module TestFixtureHelper
+    def helper_test(number)
+      number
+    end
+  end
+
+  ActiveRecord::FixtureSet.context_class.send :include, TestFixtureHelper
+end
